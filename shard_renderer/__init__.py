5681a91583a9b9877c2e580886b773d87da99d1e
WikingWarr10r <110036601+TheoGroves@users.noreply.github.com>
2026-07-01 11:55:40 +0100

    Setup UCRT64 Builds

--- shard_renderer/__init__.py
+++ shard_renderer/__init__.py
@@ -1,3 +1,16 @@
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read("renderer_config.ini")
+
+if config.getboolean("windows", "use_ucrt64_dll_path", fallback=False):
+    path = config.get("windows", "ucrt64_path", fallback="")
+
+    if path:
+        os.add_dll_directory(path)
+        print(f"[CONFIG] Added DLL directory: {path}")
+
 from shard_renderer.Release.shard_render_engine import Vec3, Mat4, translate, perspective, look_at, Engine, Camera, forward_from_euler, update_camera_vectors, Input, PlayerController
 
 __all__ = [
